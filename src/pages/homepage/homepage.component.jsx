e96c276ebf32586a3f295bc3ede9b0a5571187cb
murshidpc96 <murshid@techversantinfo.com>
2021-03-11 16:01:49 +0530

    finished styling menu-item component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import DirectorytMenu from '../../components/directory-menu/directory-menu.component';
+//scss
+import './homepage.style.scss';
+
+
+const HomePage = () => (
+    <div className="homepage">
+       <DirectorytMenu />
+    </div>
+)
+
+export default HomePage;
