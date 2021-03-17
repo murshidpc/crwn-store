65005194da2296fd095f5cf367ab08526ec29bc4
murshidpc96 <murshid@techversantinfo.com>
2021-03-17 13:39:26 +0530

    changed the collections type object to array (because we cant use map method on collections i n collection overview component)

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -4,7 +4,7 @@ const selectShop = state => state.shop;
 
 export const selectShopCollections = createSelector(
     [selectShop],
-    shop => shop.collections
+    shop => Object.keys(shop.collections).map(key => shop.collections[key])
 )
 
 export const selectCollection = collectionUrlParams =>  (
